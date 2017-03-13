5afc4d16f6398acfdfeca2da5aed5bb87aead47f
Roffelmaster <larsroffel2@gmail.com>
2017-03-13 14:16:56 +0100

    Update Tempratuur.php

--- Tempratuur.php
+++ Tempratuur.php
@@ -1,34 +1,38 @@
-<form action="" method="post">
-<table>
-<tr>
-<td>Tempratuur:</td><td><input type="number" name="numberone"></td> 
-<tr>
-<tr>
-<td><input type="submit" value="Submit" /></td>
-</tr>
-</table>
-</form>
-
-<?php
-$one = $_POST['numberone'];
-
-if ($one < 5 && $one != NULL)
-{
-echo "Het is veel te koud om buiten te spelen";
-}
-elseif ($one >= 18 && $one < 28)
-{
-echo "De korte broek kan uit de kast";
-}
-elseif ($one >= 28 && $one != NULL )
-{
-echo "Pak de koelbox maar in we gaan naar het strand";
-}
-
-else
-{
-echo "Wat is PHP toch leuk";
-}
-
 
 ?>
+0a1,34
+> <form action="" method="post">
+> <table>
+> <tr>
+> <td>Tempratuur:</td><td><input type="number" name="numberone"></td> 
+> <tr>
+> <tr>
+> <td><input type="submit" value="Submit" /></td>
+> </tr>
+> </table>
+> </form>
+> 
+> <?php
+> $one = $_POST['numberone'];
+> 
+> if ($one < 5 && $one != NULL)
+> {
+> echo "Het is veel te koud om buiten te spelen";
+> }
+> elseif ($one >= 18 && $one < 28)
+> {
+> echo "De korte broek kan uit de kast";
+> }
+> elseif ($one >= 28 && $one != NULL )
+> {
+> echo "Pak de koelbox maar in we gaan naar het strand of niet dan";
+> }
+> 
+> else
+> {
+> echo "Wat is PHP toch leuk";
+> }
+> 
+> 
+> ?>
+\ No newline at end of file
